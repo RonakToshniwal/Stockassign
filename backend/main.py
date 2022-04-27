0cde557ee5c5c5c1b1d426ec20cfb90d4c8fb92b
Chirag Tejwani <tejwanichirag@gmail.com>
2022-04-27 14:34:18 +0530

    added stock details route

--- backend/main.py
+++ backend/main.py
@@ -1,6 +1,8 @@
 import sqlite3
+from urllib import response
 from flask_cors import CORS
 from flask import Flask, jsonify,make_response,render_template,request, url_for,redirect
+import requests
 
 con = sqlite3.connect('users.db')
 print("Database connected")
@@ -12,7 +14,7 @@ con.close()
 app=Flask(__name__)
 CORS(app)
 
-
+key = "88e143bda89b4424aef8a3ad4dd67abf"
 
 @app.route('/')
 def index():
@@ -32,7 +34,16 @@ def addUser() :
         msg = "Data added successfully"
         con.close()
 
-        return jsonify( {"message" : msg} )
+        response =  jsonify( {"message" : msg} )
+        response.headers.add('Access-Control-Allow-Origin', '*')
+        return response  
+
+@app.route('/stock/<name>/', methods = ['GET'])
+def stockDetails(name) :
+    url = f"https://api.twelvedata.com/quote?symbol={name}&apikey={key}"
+    response = requests.get(url).json()
+    return response
+
 
 if __name__ == '__main__':
     app.run(debug=True)
